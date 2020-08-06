36333e0ca371c12b12b3cba1823a80d77481bd6b
jinnatul <morolswediu@gmail.com>
2020-08-07 02:18:52 +0600

    render error page & secure req

--- app.js
+++ app.js
@@ -5,9 +5,32 @@ const app = express();
 const flash    = require('connect-flash');
 const cookieParser = require('cookie-parser');
 const session      = require('express-session');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const bodyParser = require('body-parser');
 const appRouter = require('./router/appRouter');
 const authRouter = require('./router/authRouter');
+
+/******* Middlewares List ******/ 
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Limit requests from same API
+let limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, Please try again in an hour !!!'
+});
+app.use('/api', limiter);
+
+// Data sanitization against NoSQL query injections
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // handle headers
 app.use(cors());
@@ -32,6 +55,14 @@ require('./router/basicRouter')(app);
 app.use('/api', appRouter);
 app.use('/auth', authRouter);
 
-
+// error session
+app.use(function(req, res, next) {
+  next(err);
+});
+app.use(function(err, req, res, next) {
+  res.header('Content-Type', 'text/html');
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
